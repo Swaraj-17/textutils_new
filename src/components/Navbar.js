7dd6f8f8228cbce71a0638aca32776696822c561
Swaraj-17 <119113330+Swaraj-17@users.noreply.github.com>
2023-02-05 17:24:53 +0530

    temp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>  
     <div className="container-fluid">
       {/*<Link className="navbar-brand" to="/">{props.title}</Link>*/}
-      <a className="navbar-brand" href="#">{props.title}</a>
+      <a className="navbar-brand" href="/">{props.title}</a>
       <button
         className="navbar-toggler"
         type="button"
@@ -23,7 +23,7 @@ export default function Navbar(props) {
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
             {/*<Link className="nav-link active" aria-current="page" to="/">Home</Link>*/}            
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li className="nav-item">
             {/*<Link className="nav-link" to="/about">
